4b58186159b63776caf492215d481b79501c83d9
Petri-Hub <fernando.petri01@gmail.com>
2026-05-16 14:41:31 +0000

    feat: restrict access via email again

--- terraform/cloudflare/main.tf
+++ terraform/cloudflare/main.tf
@@ -34,3 +34,27 @@ resource "cloudflare_zone_setting" "ssl" {
   setting_id = "ssl"
   value      = "flexible"
 }
+
+resource "cloudflare_zero_trust_access_application" "services" {
+  for_each = { for svc in var.services : svc.name => svc }
+
+  account_id       = var.cloudflare_account_id
+  name             = each.value.name
+  domain           = each.value.name
+  type             = "self_hosted"
+  session_duration = "24h"
+
+  policies = [
+    {
+      name     = "Email OTP"
+      decision = "allow"
+      include = [
+        for email in var.authorized_emails : {
+          email = {
+            email = email
+          }
+        }
+      ]
+    }
+  ]
+}
